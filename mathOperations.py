e76ed09712f6c1e9ef9d7176cc8d0b69c1f84a6c
Ein-Bar Surie <77734536+EB-303@users.noreply.github.com>
2022-11-03 16:22:25 +0200

    Add files via upload

---
+++ mathOperations.py
@@ -0,0 +1,17 @@
+# ~~~~~~ Math Operations ~~~~~~
+
+" define 2 different variables with numbers between 1 to 20 - int type"
+
+#num1 = 
+#num2 =
+
+""" addition ->  with + operator
+    subtraction -> with - operator
+    multiplication -> with * operator
+    division -> with / operator
+    power -> with ** operator  
+    reminder -> with % operator """
+
+
+# Exe. 1
+" print the result of each operation "
