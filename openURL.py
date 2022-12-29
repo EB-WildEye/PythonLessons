7acdf157653c032c51a8f4dc8f36ce54e7a2852e
Ein-Bar Surie <77734536+EB-303@users.noreply.github.com>
2022-12-29 20:33:01 +0200

    Add files via upload

---
+++ openURL.py
@@ -0,0 +1,9 @@
+# create function that get url from the user and open it 
+import webbrowser # import webbrowser module
+
+
+def open_url(url):
+    webbrowser.open(url)
+
+
+open_url("https://github.com/EB-303/PythonLessons/blob/main/Tk_userlogin.py")
